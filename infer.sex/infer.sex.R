49f1fb84b82258ba6e5cf8adc2cd3949a6844154
xuxocast <jcastrejon@ciencias.unam.mx>
2024-08-07 11:41:20 +1000

    restore infersex

--- infer.sex/infer.sex.R
+++ infer.sex/infer.sex.R
@@ -140,15 +140,13 @@ W.sex <- function(gl, system = NULL){
   n0.w <- rowSums(w == 0 | w == 2 | w == 1, na.rm = TRUE)
   n1.w <- rowSums(w == 3, na.rm = TRUE)
   
-if(FALSE){
   # Calculate proportion
   sex.score <- function(f, m){
     return( (-f+m)/(f+m) )
   }
   
   c2 <- sex.score(n0.w, n1.w)
-}
-
+  
   if(system == 'xy'){
     lab0 <- 'M'
     lab1 <-'F'
@@ -157,7 +155,7 @@ if(FALSE){
     lab1 <- 'M'
   }
   
-  W.sex <- ifelse(n0.w < 3, lab1, lab0)
+  W.sex <- ifelse(c2 < 0, lab0, lab1)
   
   Y <- data.frame(W.sex, n0.w, n1.w)
   return(Y)
